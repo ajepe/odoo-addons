cb0899b9d7b24aa8f476732b697e48012ebff441
ajepe <ajepebabatope@gmail.com>
2019-02-02 15:17:08 +0100

    [UPD] update

--- multi_module_uninstall/models/models.py
+++ multi_module_uninstall/models/models.py
@@ -12,16 +12,6 @@ class Module(models.Model):
             including the deletion of all database structures created by the module:
             tables, columns, constraints, etc.
         """
-        mods = self.browse(self.env.context.get('active_ids'))
-        modules = mods.mapped('name')
-        self.env['ir.model.data']._module_data_uninstall(modules)
-        mods.write({'state': 'uninstalled', 'latest_version': False})
-        print(modules)
+        modules = self.browse(self.env.context.get('active_ids'))
+        [module.button_immediate_uninstall() for module in modules if module not in ['base', 'web']]
 
-    # @api.multi
-    # def module_uninstall(self):
-
-    #     modules_to_remove = self.mapped('name')
-    #     
-    #     self.write({'state': 'uninstalled', 'latest_version': False})
-    #     return True
