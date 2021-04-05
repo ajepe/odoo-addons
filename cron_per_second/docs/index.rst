288e0a363cb79b7e52bb80faea52b1d9289948be
Babatope Ajepe <ajepebabatope@gmail.com>
2021-04-05 19:02:41 +0000

    [ADD] add cron_per_sec

---
+++ cron_per_second/docs/index.rst
@@ -0,0 +1,6 @@
+By default Odoo cron job or schedule action cannot be used to schedule a job in seconds interval. 
+i.e You cannot schedule a cron job to run every 5 seconds.
+
+This module enable running cron to the lowest time fragment. 
+
+It is advisable to be use for quick and fast action that will take less execution time
---
+++ cron_per_second/models/ir_cron.py
@@ -0,0 +1,13 @@
+import odoo
+from dateutil.relativedelta import relativedelta
+
+from odoo import models, fields
+
+odoo.service.server.SLEEP_INTERVAL = 1
+odoo.addons.base.models.ir_cron._intervalTypes["seconds"] = lambda interval: relativedelta(seconds=interval)
+
+
+class IRCron(models.Model):
+    _inherit = "ir.cron"
+
+    interval_type = fields.Selection(selection_add=[("seconds", "Seconds")])
